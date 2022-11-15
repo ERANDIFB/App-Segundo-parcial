129a69fb66a2f16647f44acea4892085ed0f97cd
Connectech7 <Connectech7@gmail.com>
2022-11-15 02:47:36 -0600

    CambioLista

--- app/src/main/java/com/example/juntohtml/Jungle1.java
+++ app/src/main/java/com/example/juntohtml/Jungle1.java
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Jungle1 extends AppCompatActivity {
+    private ListView listView;
+    private List<MyInfo1> list;
+    private int []imagen = { R.drawable.img1,R.drawable.img2,R.drawable.img3,R.drawable.img4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,33 @@ public class Jungle1 extends AppCompatActivity {
         TextView Nombre;
 
         Nombre = findViewById(R.id.NombreUsr);
+
+        listView = (ListView) findViewById(R.id.listViewId1);
+        list = new ArrayList<MyInfo1>();
+        Intent intent = getIntent();
+
+        for (int i = 0; i < 4; i++){
+            MyInfo1 myData = new MyInfo1();
+            myData.setContra(String.format("Contraseña: %d",(int)(Math.random()*10000)));
+
+            if (i == 0){
+                myData.setName(String.format( "AMDGMAAF"));
+                myData.setImage(imagen[0]);
+            }
+            if (i == 1){
+                myData.setName(String.format( "IOADK"));
+                myData.setImage(imagen[1]);
+            }
+            if (i == 2){
+                myData.setName(String.format( "TDIH" ));
+                myData.setImage(imagen[2]);
+            }
+            if (i == 3){
+                myData.setName(String.format( "TGWCT" ));
+                myData.setImage(imagen[3]);
+            }
+            list.add(myData);
+        }
 
 
 
@@ -36,8 +70,17 @@ public class Jungle1 extends AppCompatActivity {
             Nombre.setText("Bienvenid@ a la app, " +Data.getNombreId() );
         } catch(Exception e){
         }
-
-
+        MyAdapter myAdapter = new MyAdapter(list, getBaseContext());
+        listView.setAdapter(myAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                toast( i );
+            }
+        });
     }
-
+    private void toast(int i){
+        Toast.makeText(getBaseContext(), list.get(i).getContra(),Toast.LENGTH_SHORT).show();
     }
+}
